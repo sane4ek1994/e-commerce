cd3469c7bdec73c8956e72965b52a0e0a3a78d02
Alexandr Naum <mfq07k9cz@gmail.com>
2023-01-24 20:24:06 +0300

    created header and footer layout component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header, Footer } from './components'
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <Header />
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
