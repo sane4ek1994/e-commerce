9ac7209a52395dc412f1ca10692b87ce7a4c9657
Alexandr Naum <mfq07k9cz@gmail.com>
2023-01-25 13:25:04 +0300

    added Items and item component + App component to class

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
-import { Header, Footer } from './components'
+import React from 'react'
 
-function App() {
-  return (
-    <div className='wrapper'>
-      <Header />
+import { Header, Footer, Items } from './components'
 
-      <Footer />
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стул серый',
+          img: 'chair-grey.jpeg',
+          descr: 'lorem lorem lorem lorem',
+          categories: 'chairs',
+          price: 49.9
+        },
+        {
+          id: 2,
+          title: 'Стол',
+          img: 'chair-grey.jpeg',
+          descr: 'lorem lorem lorem lorem',
+          categories: 'chairs',
+          price: 49.9
+        },
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'chair-grey.jpeg',
+          descr: 'lorem lorem lorem lorem',
+          categories: 'chairs',
+          price: 49.9
+        },
+        {
+          id: 4,
+          title: 'Лампа',
+          img: 'chair-grey.jpeg',
+          descr: 'lorem lorem lorem lorem',
+          categories: 'chairs',
+          price: 49.9
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className='wrapper'>
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App
